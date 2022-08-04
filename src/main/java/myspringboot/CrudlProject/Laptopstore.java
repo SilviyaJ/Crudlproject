aec001fa319a0def24bf0607fdbbafb9e2149941
SilviyaJ <silviyajoshua22@gmail.com>
2022-08-04 14:33:11 +0530

    port added

--- src/main/java/myspringboot/CrudlProject/Laptopstore.java
+++ src/main/java/myspringboot/CrudlProject/Laptopstore.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin (origins ="http://localhost:3000")
 @RestController
 public class Laptopstore {
  
